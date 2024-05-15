e669144438124ab0f544b5c40c124f20be99f56c
Asiman <asi.teymur@gmail.com>
2024-05-16 01:13:02 +0400

    uc dene bosluq

--- src/EXgeneral/exgeneral.kt
+++ src/EXgeneral/exgeneral.kt
@@ -1,16 +1,67 @@
 package EXgeneral
-
-fun main() {
+//1
+/*fun main() {
     var numbers = arrayListOf(3, 3, 7, 17)
-    var x = 0
+    var result = ortalama(numbers)
+    println(result)
 
-    println(ortalama)
 }
 
-    fun ortalama (x, array: ArrayList<Int>) {
-        while (i in array)
+    fun ortalama ( array: ArrayList<Int>): Double {  //burda Double yazanda xeta verir
 
-        var result = (x+i)/4
+        var total = 0.0
+        for (i in array) {
+            total += i}
+            return total/array.size
 
-    } i +=1
+    }*/
+//2
+    /*fun main() {
+    val numbers = arrayListOf(1, 4, 10, 5, 29, 60)
+      var result = findMaxNumber(numbers)
+    println(result)
 
+}
+
+    fun findMaxNumber ( array: ArrayList<Int>): Int {
+
+        var largest = array[0]
+
+        for (i in array) {
+
+            if (i > largest){
+                largest = i
+                }
+
+        }
+        return largest
+    }*/
+
+/*fun main() {
+    isPrime(5)
+
+}
+
+fun isPrime ( number: Int) {
+
+        if (number%2 !==0) {
+            println("eded tekdir")
+        }else
+        println("eded cutdur")
+}*/
+fun main() {
+    println(calculateGrade(80))
+
+}
+
+fun calculateGrade  ( point: Int) {
+
+    when (point) {
+        in 90..100 -> println ("A")
+        in 80..90 -> println ("B")
+        in 60..80 -> println ("C")
+        in 0..60 -> println ("D")
+
+}
+
+}
