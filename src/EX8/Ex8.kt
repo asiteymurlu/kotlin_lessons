5c80636d5674ddcb88f4ea0aafa2f054aa632563
AsimanT <asi.teymur@gmail.com>
2024-05-16 18:05:25 +0400

    elave

--- src/EX8/Ex8.kt
+++ src/EX8/Ex8.kt
@@ -1,5 +1,60 @@
 package EX8
 
-fun main() {
+//1fun main() {
 
+
+/*    var foodPrice = hashMapOf<String, Int>()
+
+    foodPrice["Bread"] = 50
+    foodPrice["Milk"] = 100
+    foodPrice["Apple"] = 25
+    foodPrice["Pepper"] = 2
+
+    control(22, foodPrice)
 }
+
+    fun control (number: Int, map:HashMap<String, Int>) {
+        for (i in map.values) {
+            if (number == i) {
+                println("Deyer movcuddur") }
+                else
+                    println("Deyer movcud deyil")
+           } }*/
+
+//2
+
+    fun main () {
+    val numbers = arrayListOf(1,2,1,0,12,3,25,12,0,36,36)
+
+
+
+    }
+
+    fun findRepeatingElements(array: ArrayList<Int>) {
+        var reapited = arrayListOf(i)
+        for (i in array) {
+            if (i == i)
+                println(reapited)
+        }
+    }
+
+ //3
+
+    /*fun main() {
+
+
+      println("Yasinizi daxil edin:")
+    var age = readLine()!!.toInt()
+    agePeriod(age)
+    }
+
+  fun agePeriod (number: Int) {
+
+      when {
+          number in 1..17 -> println("Usaq")
+          number in 18..25 -> println("Genc")
+          number in 26..40 -> println("Cavan")
+          number > 40 -> println("Qoca")
+          else -> println("duzgun deyer daxil edin!")
+      }
+  }*/
